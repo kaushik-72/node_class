595e56bbce1bdabd9a589f0e54615ce4cf030f5a
kaushik-72 <kaushikkarmakarsmart@gmail.com>
2026-02-18 23:01:44 +0530

    fs inital

---
+++ D02/fs.js
@@ -0,0 +1,19 @@
+import fs from "node:fs";
+
+// console.log(fs);
+
+console.log(1);
+fs.writeFileSync("")
+
+
+
+
+
+// console.log("hey there");
+
+// function copyPaste(){
+//     let contents = fs.readFileSync(path:"./fs.js",options:"utf-8");
+//     fs.writeFileSync()
+// };
+
+
